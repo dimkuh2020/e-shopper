67e6e9cf28383b2073f602702b80786bc9cf481e
dimkuh <dimkuh1985@gmail.com>
2019-12-16 17:37:43 +0200

    admin category

---
+++ modules/admin/models/Category.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property int $parent_id
+ * @property string $name
+ * @property string $keywords
+ * @property string $description
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    public function getCategory(){
+        return $this->hasOne(Category::className(), ['id' => 'parent_id']); // связь в одной таблице category по parent
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id'], 'integer'],
+            [['name'], 'required'],
+            [['name', 'keywords', 'description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'Номер категории',
+            'parent_id' => 'Родительская категори',
+            'name' => 'Название',
+            'keywords' => 'Ключевые слова',
+            'description' => 'мета-описание',
+        ];
+    }
+}
